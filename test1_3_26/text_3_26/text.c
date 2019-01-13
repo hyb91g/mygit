e5d7cfbf4f2a4a10b06feb412abf7a69441f50eb
hyb91g <958592337@qq.com>
2019-01-13 19:18:44 +0800

    给定两个整形变量的值，将两个值的内容进行交换

---
+++ test1_3_26/text_3_26/text.c
@@ -0,0 +1,19 @@
+//给定两个整形变量的值，将两个值的内容进行交换。
+
+#include<stdio.h>
+
+int main ()
+{
+	int i = 5;
+	int j = 12;
+	int k = 0;
+
+	k = i;
+	i = j;
+	j = k;
+	
+	printf("%d  %d", i, j);
+	
+
+	return 0;
+}
