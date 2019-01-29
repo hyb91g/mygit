b45cbb831fcecfa52751aec370c2ab8ae808ecf1
hyb91g <958592337@qq.com>
2019-01-29 14:15:37 +0800

    简单的加减乘除计算器

---
+++ test5_5_23/test5_5_23/test.c
@@ -0,0 +1,80 @@
+//计算器
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int Add(int a, int b)
+{
+	return a + b;
+}
+int Sub(int a, int b)
+{
+	return a - b;
+}
+int Mul(int a, int b)
+{
+	return a * b;
+}
+int Div(int a, int b)
+{
+	return a / b;
+}
+
+void menu()
+{
+	printf("****************************\n");
+	printf("***** 1.add      2.sub *****\n");
+	printf("***** 3.mul      4.div *****\n");
+	printf("******     0.exit      *****\n");
+	printf("****************************\n");
+}
+
+void calc(int (*pfun) (int, int))
+{
+	int x = 0;
+	int y = 0;
+	int ret = 0;
+	printf("输入两个数：");
+	scanf_s("%d%d", &x, &y);
+	ret = pfun(x, y);
+	printf("ret = %d\n", ret);
+}
+int main()
+{
+	int input = 1;
+	int(*p[5])(int, int) = { 0, Add, Sub, Mul, Div };
+	while (input)
+	{
+		menu();
+		printf("请选择：");
+		scanf_s("%d", &input);
+		if (input >= 1 && input <= 4)
+		{
+			calc(p[input]);
+		}
+		
+		//switch (input)
+		//{
+		//case 1:
+		//	ret = Add(x, y);
+		//	break;
+		//case 2:
+		//	ret = Sub(x, y);
+		//	break;
+		//case 3:
+		//	ret = Mul(x, y);
+		//	break;
+		//case 4:
+		//	ret = Div(x, y);
+		//	break;
+		//case 0:
+		//	printf("退出\n");
+		//	break;
+		//default:
+		//	printf("输入错误\n");
+		//	break;
+		//}
+	}
+	system("pause");
+	return 0;
+}
