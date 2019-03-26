f97fd4ee574cde58980d60f11840a803a8b1dff4
hyb91g <958592337@qq.com>
2019-03-26 13:32:34 +0800

    将一个字符串中的每个空格替换成“%20”

---
+++ replaceSpace/test.c
@@ -0,0 +1,48 @@
+//请实现一个函数，将一个字符串中的每个空格替换成“%20”。
+
+#include <stdio.h>
+#include <string.h>
+
+void replaceSpace(char *str, int length) {
+	int count = 0;
+	int i = 0;
+	for (; i < length; i++) {
+		if (str[i] == ' ') {
+			count++;
+		}
+	}
+	if (count == 0) {
+		return;
+	}
+	int newlength = length + count * 2;
+
+	int tail = newlength - 1;
+	int sectail = length - 1;
+
+	str[tail] = '\0';
+
+	while (1) {
+		if (str[sectail] == ' ') {
+			str[tail--] = '0';
+			str[tail--] = '2';
+			str[tail--] = '%';
+			sectail--;
+			count--;
+			if (count == 0) {
+				return;
+			}
+		}
+		else {
+			str[tail--] = str[sectail--];
+		}
+	}
+
+
+}
+int main()
+{
+	char str[100] = "fds fsdf sf ";
+	replaceSpace(str, strlen(str) + 1);
+	printf("%s", str);
+	return 0;
+}
